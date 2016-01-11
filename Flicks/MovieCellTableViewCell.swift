922b7620f3a058ff5213c2a5d0c51a1ed06663ed
Jeremy <jeremylehman9@gmail.com>
2016-01-11 10:07:41 -0500

    finished quick start #2

---
+++ Flicks/MovieCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCellTableViewCell.swift
+//  Flicks
+//
+//  Created by Erin Walsh on 1/11/16.
+//  Copyright © 2016 Jeremy Lehman. All rights reserved.
+//
+
+import UIKit
+
+class MovieCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
